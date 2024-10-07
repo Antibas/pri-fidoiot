9523bd6236d0435f79f3817f54f48db46b545e08
p.antivasis@pasiphae.eu <p.antivasis@pasiphae.eu>
2024-10-07 17:45:43 +0300

    Added more more APIs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Response
+from fastapi import FastAPI, APIRouter, Response, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import subprocess, os, aiomysql, requests
@@ -106,36 +106,42 @@ async def stop_all_components():
     except Exception as e:
         return Response(content=e.__str__(), status_code=500)
 
-@router.post("/rvinfo")
+@router.get("/rvinfo")
 async def create_rv_info():
-    res = requests.post(url="http://localhost:8039/api/v1/rvinfo", headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'), data='[[[5,"host.docker.internal"],[3,8041],[12,2],[2,"127.0.0.1"],[4,8041]]]')
-    data = res.content
-    return data
+    return requests.post(url="http://localhost:8039/api/v1/rvinfo", headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'), data='[[[5,"host.docker.internal"],[3,8041],[12,2],[2,"127.0.0.1"],[4,8041]]]')
 
 @router.get("/certificate")
 async def get_certificate(alias: str='SECP256R1'):
-    res = requests.get(url="http://localhost:8042/api/v1/certificate", params={"alias": alias}, headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
-    data = res.content
-    return data
+    return requests.get(url="http://localhost:8042/api/v1/certificate", params={"alias": alias}, headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
 
 @router.get("/deviceinfo/{seconds}")
 async def get_device_info(seconds: int):
-    res = requests.get(url=f"http://localhost:8039/api/v1/deviceinfo/{seconds}", headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
-    data = res.content
-    return data
+    return requests.get(url=f"http://localhost:8039/api/v1/deviceinfo/{seconds}", headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
 
-@router.get("/vouchers/{deviceSerialNo}")
-async def pem_cert(deviceSerialNo: str, owner_cert: str):
-    res = requests.post(url=f"http://localhost:8039/api/v1/mfg/vouchers/{deviceSerialNo}", data=owner_cert, headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
-    data = res.content
-    return data
+@router.post("/vouchers/{deviceSerialNo}")
+async def pem_cert(deviceSerialNo: str, owner_cert: dict):
+    return requests.post(url=f"http://localhost:8039/api/v1/mfg/vouchers/{deviceSerialNo}", data=owner_cert, headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
 
 @router.get("/vouchers")
 async def pem_cert_owner(owner_cert: str):
-    res = requests.post(url=f"http://localhost:8039/api/v1/owner/vouchers", data=owner_cert, headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
-    data = res.content
-    return data
+    return requests.post(url=f"http://localhost:8042/api/v1/owner/vouchers", data=owner_cert, headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
 
+@router.get("/redirect")
+async def owner_redirect():
+    return requests.post(url=f"http://localhost:8042/api/v1/owner/redirect", data='[[null,"host.docker.internal",8043,5]]', headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
+
+
+@router.get("/to0/{guid}")
+async def to0(guid: str):
+    return requests.get(url=f"http://localhost:8042/api/v1/to0/{guid}", headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
+
+@router.get("/rvreplacement")
+async def replace_rv():
+    return requests.post(url=f"http://localhost:8042/api/v1/owner/rvreplacement", data='[[[5,"host.docker.internal"],[3,8041],[12,2],[2,"127.0.0.1"],[4,8041]]]', headers={"Content-Type": 'text/plain'}, auth=('apiUser','1234'))
+
+@router.get("/svi")
+async def configure_owner_service():
+    return requests.post(url=f"http://localhost:8042/api/v1/owner/svi", data=[{"filedesc" : "setup.sh", "resource" : "https://google.com"},{"exec" : ["sh","setup.sh"] }], auth=('apiUser','1234'))
 
 @router.get("/db/{database_name}/{table_name}")
 async def get_database(database_name: str, table_name: str):
